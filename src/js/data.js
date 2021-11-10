6d5bd179fe6e492f8ccb77236a2b592e4dbd3d16
Dmytro Osiichuk <dmytro.osiychuk@gmail.com>
2021-11-10 16:38:34 +0100

    Add handlebars to display books

--- src/js/data.js
+++ src/js/data.js
@@ -57,3 +57,4 @@ dataSource.books = [
     }
   },
 ];
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
+/* global Handlebars, utils, dataSource */
+
 {
-  'use stict';
+  'use strict';
+  
   const select = {
     templateOf: {
       book: '#template-book',
+    },
+    containerOf: {
+      books: '.books-list',
     },
   };
   const templates = {
@@ -10,4 +16,16 @@
       document.querySelector(select.templateOf.book).innerHTML
     ),
   };
+  function renderBooks(dataSource) {
+    dataSource.books.forEach(book => {
+      const generatedHTML = templates.book(book);
+      const domElement = utils.createDOMFromHTML(generatedHTML);
+      const bookContainer = document.querySelector(select.containerOf.books);
+      bookContainer.appendChild(domElement);
+    });
+  }
+
+  renderBooks(dataSource);
+
+
 }
